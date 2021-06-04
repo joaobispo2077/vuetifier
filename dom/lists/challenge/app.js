a0d2145a9571bcd0218224b5d60851c6763450be
João Bispo <joaobispo2077@gmail.com>
2021-06-04 11:21:19 -0300

    feat: win challenge render lists

---
+++ dom/lists/challenge/app.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: '#challenge',
+	data: {
+		showText: true,
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		book: {
+			title: 'O Senhor dos Anéis',
+			author: 'J.R.R. Tolkiens',
+			chapter: '3'
+		},
+		student: {
+			id: 10,
+			name: 'Maria',
+			notes: [7.67, 8.33, 6.98, 9.21]
+		}
+	}
+});
