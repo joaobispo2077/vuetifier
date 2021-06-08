624c8a34d8116a20f1d11a22420ec8bc4b63ea62
João Bispo <joaobispo2077@gmail.com>
2021-06-08 10:33:32 -0300

    feat: added prop types into component

--- components/comunication/src/components/UserInfo.vue
+++ components/comunication/src/components/UserInfo.vue
@@ -2,13 +2,23 @@
     <div class="box">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do Usuário: <strong> {{name}} </strong> </p>
+        <p>Nome do Usuário: <strong> {{ invertText() }} </strong> </p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['name']
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        invertText() {
+           return this.name.split('').reverse().join('');
+        }
+    }
 }
 </script>
 
