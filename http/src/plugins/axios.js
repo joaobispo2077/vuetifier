df6a9aa22101d72e7e4dc41c800522478da4641d
João Bispo <joaobispo2077@gmail.com>
2021-07-05 14:46:02 -0300

    chore: inject axios instance as vue plugin

--- http/src/plugins/axios.js
+++ http/src/plugins/axios.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-axios.defaults.baseURL = '';
+// axios.defaults.baseURL = process.env.VUE_APP_FIREBASE_URL;
 
 Vue.use({
   install(Vue) {
-    Vue.prototype.$http = axios;
+    // Vue.prototype.$http = axios;
+    Vue.prototype.$http = axios.create({
+      baseURL: process.env.VUE_APP_FIREBASE_URL
+    });
   }
 })
