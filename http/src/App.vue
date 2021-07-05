893f34230e11e0edf944c20df83b5d3ff035159e
João Bispo <joaobispo2077@gmail.com>
2021-07-05 15:23:07 -0300

    feat: create and remove users

--- http/src/App.vue
+++ http/src/App.vue
@@ -1,11 +1,61 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input
+					type="text"
+					size="lg"
+					v-model='user.name'
+					placeholder="Insira o seu nome"
+				/>
+			</b-form-group>
+			<b-form-group label="E-mail:">
+				<b-form-input
+					type="text"
+					size="lg"
+					v-model='user.email'
+					placeholder="Insira o seu e-mail"
+				/>
+			</b-form-group>
+			<hr>
+			<b-button @click="submit" size="lg" variant="primary">Salvar</b-button>
+			<b-button class="ml-2" @click="getUsers" size="lg" variant="success">Listar usuários</b-button>
+		</b-card>
+		<hr>
+		<b-list-group>
+			<b-list-group-item v-for="(user, id) in users" :key='id'>
+				<strong>Nome: {{user.name }}</strong> <br/>
+				<strong>E-mail: {{user.email }}</strong>  <br/>
+				<strong>ID: {{ id }}</strong>
+			</b-list-group-item>
+		</b-list-group>
 	</div>
 </template>
 
 <script>
 export default {
+	data: () => ({
+		users: [],
+		user: {
+			name: '',
+			email: ''
+		}
+	}),
+	methods: {
+		submit() {
+			this.$http.post('users.json', this.user).then(response => {
+				this.user.name ='';
+				this.user.email ='';
+			})
+		},
+		getUsers() {
+			this.$http.get('users.json').then(response => {
+				this.users = response.data;
+				console.log(this.users);
+			})
+		}
+	}
 	// created() {
 	// 	this.$http.post('users.json', {
 	// 		name: 'Joao',
