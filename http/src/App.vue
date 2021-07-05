a6490124fad23ae3a4908c3fb95242363f84e9c5
João Bispo <joaobispo2077@gmail.com>
2021-07-05 14:42:59 -0300

    feat: setup http plugin with axios

---
+++ http/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app" class="container">
+		<h1>HTTP com Axios</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	// created() {
+	// 	this.$http.post('users.json', {
+	// 		name: 'Joao',
+	// 		email: 'joaobispo2077@gmail.com'
+	// 	}, {
+
+	// 		headers: {
+	// 			Accept: "application/json",
+	// 			"Content-Type": "application/json",
+	// 			'Access-Control-Allow-Origin': '*'
+	// 		}
+	// 	}).then(response => console.log(response));
+	// }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 50px;
+}
+</style>
