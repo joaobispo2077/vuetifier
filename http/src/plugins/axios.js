c57305b64cb7026b599448fb30859d84df4c6822
João Bispo <joaobispo2077@gmail.com>
2021-07-05 15:23:42 -0300

    feat: add axios method that intercept each request

--- http/src/plugins/axios.js
+++ http/src/plugins/axios.js
@@ -6,8 +6,20 @@ import axios from 'axios';
 Vue.use({
   install(Vue) {
     // Vue.prototype.$http = axios;
-    Vue.prototype.$http = axios.create({
+    const api = axios.create({
       baseURL: process.env.VUE_APP_FIREBASE_URL
     });
+
+    api.interceptors.request.use(async config => {
+      await new Promise(resolve => {
+
+        setTimeout(() => resolve(true), 2500);
+      });
+
+      console.log('config', config)
+      return config;
+    })
+
+    Vue.prototype.$http = api;
   }
 })
